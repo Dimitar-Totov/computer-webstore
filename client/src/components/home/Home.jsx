b88623178ea8fb92aaca1e0576a59fdcae62cadf
Dimitar-Totov <dimitar.totov@abv.bg>
2025-09-23 14:32:11 +0300

    Add some style to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -36,16 +36,12 @@ export default function Home() {
                         <p>Today's Best Deals</p>
                         <div className={style["cards"]}>
                             <NavLink to={"/product"}>
-                                <div>
-                                    <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc4/MSI-Cyborg-laptop.png" alt="" />
-                                    <p>MSI Cyborg - 15.6" GeForce RTX 4050 Laptop GPU - Intel i7-13620H - 16GB Memory - 512 GB SSD - Windows 11 Home - Gaming Laptops - 144 Hz IPS (A13VE-218US )</p>
-                                </div>
+                                <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc4/MSI-Cyborg-laptop.png" alt="" />
+                                <p>MSI Cyborg - 15.6" GeForce RTX 4050 Laptop GPU - Intel i7-13620H - 16GB Memory - 512 GB SSD - Windows 11 Home - Gaming Laptops - 144 Hz IPS (A13VE-218US )</p>
                             </NavLink>
                             <NavLink to={"/product"}>
-                                <div>
-                                    <img src="https://assets3.razerzone.com/dWiYYPxIjKjjzjTe2x8nNO_y-Ko=/300x300/https%3A%2F%2Fmedias-p1.phoenix.razer.com%2Fsys-master-phoenix-images-container%2Fheb%2Fh5a%2F9822063231006%2Fbasilisk-v3-pro-35k-white-500x500.png" alt="" />
-                                    <p>Razer Basilisk V3 Customizable Ergonomic Gaming Mouse: Fastest Gaming Mouse Switch - Chroma RGB Lighting - 26K DPI Optical Sensor - 11 Programmable Buttons - HyperScroll Tilt Wheel - Classic Black</p>
-                                </div>
+                                <img src="https://assets3.razerzone.com/dWiYYPxIjKjjzjTe2x8nNO_y-Ko=/300x300/https%3A%2F%2Fmedias-p1.phoenix.razer.com%2Fsys-master-phoenix-images-container%2Fheb%2Fh5a%2F9822063231006%2Fbasilisk-v3-pro-35k-white-500x500.png" alt="" />
+                                <p>Razer Basilisk V3 Customizable Ergonomic Gaming Mouse: Fastest Gaming Mouse Switch - Chroma RGB Lighting - 26K DPI Optical Sensor - 11 Programmable Buttons - HyperScroll Tilt Wheel - Classic Black</p>
                             </NavLink>
                             <NavLink to={"/product"}>
                                 <img src="https://storage-asset.msi.com/global/picture/image/feature/Gaming-Gear/Combo/gk100-combo/msi-gk100-combo-hotkey-new.png" alt="" />
@@ -61,13 +57,26 @@ export default function Home() {
                 <div className={style["best-sellers"]}>
                     <p>Best Sellers</p>
                     <div className={style["best-sellers-cards"]}>
-                        <NavLink to={"/product"}> <img src="https://i.pcmag.com/imagery/reviews/00B2H5RkSOPNPhASt8jGqLq-5-hero-image-gallery..v1744242002.jpg" alt="" /> </NavLink>
-                        <NavLink to={"/product"}> <img src="https://www.digitaltrends.com/wp-content/uploads/2024/10/Acer-Nitro-16-gaming-laptop.jpg?fit=720%2C540&p=1" alt="" /> </NavLink>
-                        <NavLink to={"/product"}> <img src="https://i5.walmartimages.com/seo/BENGOO-G9000-Stereo-Gaming-Headset-with-Noise-Cancelling-Mic-LED-Light-Bass-Surround-for-PS4-PC-Xbox-One-PS5-Controller_83e46faa-4996-42b6-92e5-991635578de8.d14c4eccccd6e0132fc52238fd918f08.jpeg" alt="" /> </NavLink>
-                        <NavLink to={"/product"}> <img src="https://www.jbhifi.com.au/cdn/shop/products/511962-Product-0-I-637497686352598307.jpg?v=1614132308" alt="" /> </NavLink>
-                        <NavLink to={"/product"}> <img src="https://xrocker.co.uk/cdn/shop/files/ascend-height-adjustable-desk-black-1_1000x1000_crop_center.jpg?v=1753873729" alt="" /> </NavLink>
-                        <NavLink to={"/product"}> <img src="https://m.media-amazon.com/images/I/81+EtD9L9uS.jpg" alt="" /> </NavLink>
-                        <NavLink to={"/product"}> <img src="https://storage-asset.msi.com/global/picture/image/feature/vga/NVIDIA/GTX1080/carousel_TF6.jpg" alt="" /> </NavLink>
+                        <NavLink to={"/product"}>
+                            <img src="https://desktop.bg/system/images/477938/normal/Aurora_R16.png" alt="" />
+                            <p>Alienware Area-51 Gaming Desktop</p>
+                        </NavLink>
+                        <NavLink to={"/product"}>
+                            <img src="https://laptop.bg/system/images/505287/original/acer_nitro_17_an1742_NHQSCEX00A.png" alt="" />
+                            <p>Acer Nitro V15 Gaming ANV15-41-R948, 15.6", Full HD, AMD Ryzen 7 7735HS (3.2/4.7GHz, 16M), NVIDIA RTX 4050 6GB GDDR6 DLSS 3, 16 GB, 512 GB SSD - NH.QSGEX.00H</p>
+                        </NavLink>
+                        <NavLink to={"/product"}>
+                            <img src="https://edifier-online.com/cdn/shop/files/2_9c2acecb-7b6b-4a73-a93d-4ffa95a83049.png?v=1709632400&width=3840" alt="" />
+                            <p>HyperX Cloud Alpha</p>
+                        </NavLink>
+                        <NavLink to={"/product"}>
+                            <img src="https://cdn.shopify.com/s/files/1/0625/5650/4319/files/leetdesk-aura-gaming-desk_Frame_Black_Tabletop-Dark_Edge-LED_Style-Classic_Size-160x80_diagonal.png?v=1718357108" alt="" />
+                            <p>Gaming RGB LED Backlit Desk Set Bright Youth Desk with Volume Control System 155cm, SNAKE 1.4</p>
+                        </NavLink>
+                        <NavLink to={"/product"}>
+                            <img src="https://desktop.bg/system/images/444629/original/asus_dual_geforce_rtx_4060_ti_advanced_edition_16gb.png" alt="" />
+                            <p>GeForce RTX 4060 8GB Infinity 2 DLSS 3 - NE64060019P1-1070L</p>
+                        </NavLink>
                     </div>
                 </div>
             </main>
